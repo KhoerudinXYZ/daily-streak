c52456164df3837bb0ddd5230209ea46fb492bca
KhoerudinXYZ <muhammk12@gmail.com>
2026-03-05 23:42:09 +0700

    add some style

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const display = document.getElementById('day');
 let num = Number(localStorage.getItem("day")) || 0;
 const updateUI = () => display.textContent = num;
+const themeL = document.getElementById('theme');
+const Stheme = 'style.css';
+const Ftheme = 'fail.css';
 updateUI();
 
 const add = () => { 
     num++; 
     localStorage.setItem("day", num);
+    theme = localStorage.setItem("href", Stheme);
+    themeL.setAttribute("href", Stheme);
     updateUI(); 
 };
 const reset = () => { 
     num = 0; 
     localStorage.setItem("day", num);
+    theme = localStorage.setItem("href", Ftheme);
+    themeL.setAttribute("href", Ftheme);
     updateUI(); 
 };
+
+// jika day == 0 change the style.css to fail.css
