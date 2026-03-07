13ef507c1663a4cb1afd093f2c63554803e4bad2
KhoerudinXYZ <muhammk12@gmail.com>
2026-03-08 02:06:10 +0700

    give more expression

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 const display = document.getElementById('day');
 let num = Number(localStorage.getItem("day")) || 0;
-const updateUI = () => display.textContent = num;
 const themeL = document.getElementById('theme');
 const Stheme = 'style.css';
 const Ftheme = 'fail.css';
+let theme = localStorage.getItem("href");
+const updateUI = () => { 
+    display.textContent = num; 
+    themeL.setAttribute("href", theme);
+}
 updateUI();
 
 const add = () => { 
     num++; 
+    theme = Stheme;
     localStorage.setItem("day", num);
-    theme = localStorage.setItem("href", Stheme);
-    themeL.setAttribute("href", Stheme);
+    localStorage.setItem("href", theme);
+    themeL.setAttribute("href", theme);
     updateUI(); 
 };
 const reset = () => { 
     num = 0; 
+    theme = Ftheme;
     localStorage.setItem("day", num);
-    theme = localStorage.setItem("href", Ftheme);
-    themeL.setAttribute("href", Ftheme);
+    localStorage.setItem("href", theme);
+    themeL.setAttribute("href", theme);
     updateUI(); 
 };
 
